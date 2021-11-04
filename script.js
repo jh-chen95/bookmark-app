4e8720574a61f4732b9826be69f0b2a7c73bdf1b
Jianhong Chen <chen_jianhong608@hotmail.com>
2021-11-04 19:54:56 -0400

    third commit

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ modalClose.addEventListener("click", () => {
 window.addEventListener("click", (event) => {
     event.target === modal ? modal.classList.remove("show-modal") : false
 });
+
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g;
+    if (!nameValue || !urlValue) {
+        alert("Please submit values for both fields.");
+        return false;
+    }
+    if (!urlValue.match(expression)) {
+        alert("Please provide a valid web address");
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Handle data from form
+function storeBookmark(event) {
+    event.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes("http://") && !urlValue.includes("https://")) {
+        urlValue = `https://${urlValue}`;
+    }
+    
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+// Event listener
+bookmarkForm.addEventListener("submit", storeBookmark);
