b9bd0c04738993ce37bc64911799a1ca6d62909f
Jianhong Chen <chen_jianhong608@hotmail.com>
2021-11-05 19:59:12 -0400

    fourth commit

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 // Show modal, focus on input
 function showModal() {
     modal.classList.add("show-modal");
@@ -38,6 +40,56 @@ function validate(nameValue, urlValue) {
     return true;
 }
 
+// Build bookmarks DOM
+function buildBookmarks() {
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const {name, url} = bookmark;
+        // Item
+        const item = document.createElement("div");
+        item.classList.add("item");
+        // Close icon
+        const closeIcon = document.createElement("i");
+        closeIcon.classList.add("fas", "fa-trash-alt");
+        closeIcon.setAttribute("title", "Delete Bookmark");
+        closeIcon.setAttribute("onclick", `deleteBookmark("${url}")`);
+        // Link container
+        const linkInfo = document.createElement("div");
+        linkInfo.classList.add("name");
+        // Favicon
+        const favicon = document.createElement("img");
+        favicon.setAttribute("src", `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute("alt", "Favicon");
+        // Link
+        const link = document.createElement("a");
+        link.setAttribute("href", `${url}`);
+        link.setAttribute("target", "_blank");
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+}
+
+// Fetch bookmarks
+function fetchBookmarks() {
+    // Get bookmarks from localStorage if available
+    if (localStorage.getItem("bookmarks")) {
+        bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    } else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: "Google",
+                url: "https://www.google.com/",
+            },
+        ];
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
 // Handle data from form
 function storeBookmark(event) {
     event.preventDefault();
@@ -46,11 +98,22 @@ function storeBookmark(event) {
     if (!urlValue.includes("http://") && !urlValue.includes("https://")) {
         urlValue = `https://${urlValue}`;
     }
-    
     if (!validate(nameValue, urlValue)) {
         return false;
     }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 // Event listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On load, fetch bookmarks
+fetchBookmarks();
